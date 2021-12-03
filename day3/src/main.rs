5731d5a9c9acdae3a4abc1be1438a769ce7a7c5b
Remo Senekowitsch <remlause@gmail.com>
2021-12-03 22:22:20 +0100

    [day 3] Improve code quality

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,97 +9,55 @@ fn part1(input: &str) {
 
     for line in input.lines() {
         for (i, c) in line.chars().enumerate() {
-            if c == '1' {
-                v[i] += 1;
-            };
+            v[i] += c.to_string().parse::<usize>().unwrap();
         }
     }
 
     let mut gamma: usize = 0;
     let mut epsilon: usize = 0;
-    for (i, num_occ) in v.into_iter().rev().enumerate() {
-        if num_occ > n / 2 {
+    for (i, num_ones) in v.into_iter().rev().enumerate() {
+        if num_ones > n / 2 {
             gamma += 1 << i;
         } else {
             epsilon += 1 << i;
         }
     }
 
-    println!("{:?}", gamma * epsilon)
+    println!("{:?}", gamma * epsilon) // 4174964
+}
+
+fn apply_bit_criteria(diagnostic_report: &Vec<&str>, flipped: bool) -> usize {
+    let mut candidates = diagnostic_report.clone();
+    let l = candidates[0].len();
+    for i in 0..l {
+        let num_ones_at_i = candidates
+            .iter()
+            .map(|line| line.chars().nth(i).unwrap())
+            .filter(|c| *c == '1')
+            .count();
+        let n = candidates.len();
+        let more_ones_or_equal = num_ones_at_i >= (n - num_ones_at_i);
+        let req_char = match more_ones_or_equal != flipped {
+            true => '1',
+            false => '0',
+        };
+        candidates = candidates
+            .into_iter()
+            .filter(|line| line.chars().nth(i).unwrap() == req_char)
+            .collect_vec();
+        if candidates.len() == 1 {
+            break;
+        }
+    }
+    return usize::from_str_radix(candidates[0], 2).unwrap();
 }
 
 fn part2(input: &str) {
-    let l = input.lines().next().unwrap().len();
+    let diagnostic_report = input.lines().collect_vec();
+    let oxygen = apply_bit_criteria(&diagnostic_report, false);
+    let co2 = apply_bit_criteria(&diagnostic_report, true);
 
-    let mut v = Vec::with_capacity(l);
-    v.resize(l, 0);
-
-    let mut candidates = input.lines().collect_vec();
-    for i in 0..l {
-        let count = candidates
-            .iter()
-            .map(|line| line.chars().nth(i).unwrap())
-            .filter(|c| *c == '1')
-            .count();
-        let n = candidates.len();
-        let req_char = {
-            if count > n / 2 {
-                '1'
-            } else if count == n / 2 && n % 2 == 0 {
-                '1'
-            } else {
-                '0'
-            }
-        };
-        candidates = candidates
-            .into_iter()
-            .filter(|line| {
-                let relevant = line.chars().nth(i).unwrap();
-                relevant == req_char
-            })
-            .collect_vec();
-
-        if candidates.len() == 1 {
-            break;
-        }
-    }
-    let oxygen = usize::from_str_radix(candidates[0], 2).unwrap();
-
-    let mut candidates = input.lines().collect_vec();
-    for i in 0..l {
-        let count = candidates
-            .iter()
-            .map(|line| line.chars().nth(i).unwrap())
-            .filter(|c| *c == '1')
-            .count();
-        let n = candidates.len();
-        let req_char = {
-            if count > n / 2 {
-                '0'
-            } else if count == n / 2 && n % 2 == 0 {
-                '0'
-            } else {
-                '1'
-            }
-        };
-        candidates = candidates
-            .into_iter()
-            .filter(|line| {
-                let relevant = line.chars().nth(i).unwrap();
-                relevant == req_char
-            })
-            .collect_vec();
-
-        if candidates.len() == 3 {
-            println!("{:?}", candidates)
-        }
-        if candidates.len() == 1 {
-            break;
-        }
-    }
-    let co2 = usize::from_str_radix(candidates[0], 2).unwrap();
-
-    println!("{:?}", oxygen * co2);
+    println!("{:?}", oxygen * co2) // 4474944
 }
 
 fn main() {
