d2ad2638f87ea6fe5ecf8ec36fb0bed05919c47c
Remo Senekowitsch <remlause@gmail.com>
2021-12-07 07:24:09 +0100

    day 7

---
+++ day7/src/main.rs
@@ -0,0 +1,86 @@
+use itertools::*;
+
+fn calc_fuel_consumption(crabs: &Vec<i32>, alignment_pos: i32) -> i32 {
+    crabs
+        .into_iter()
+        .map(|crab| (crab - alignment_pos).abs())
+        .sum()
+}
+
+fn median_crab(crabs: &Vec<i32>) -> i32 {
+    let mut clone = crabs.clone();
+    clone.sort();
+    clone[clone.len() / 2]
+}
+
+fn part1(crabs: Vec<i32>) {
+    let mut bruh = median_crab(&crabs);
+    loop {
+        let prev = calc_fuel_consumption(&crabs, bruh);
+        let next = calc_fuel_consumption(&crabs, bruh + 1);
+        if next <= prev {
+            bruh += 1
+        } else {
+            break;
+        }
+    }
+    loop {
+        let prev = calc_fuel_consumption(&crabs, bruh);
+        let next = calc_fuel_consumption(&crabs, bruh - 1);
+        if next <= prev {
+            bruh -= 1
+        } else {
+            break;
+        }
+    }
+    println!("{:?}", calc_fuel_consumption(&crabs, bruh))
+}
+
+fn calc_fuel_consumption_2(crabs: &Vec<i32>, alignment_pos: i32) -> i32 {
+    crabs
+        .into_iter()
+        .map(|crab| {
+            let diff = (crab - alignment_pos).abs();
+            if diff > 0 {
+                (1..=diff).sum()
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn part2(crabs: Vec<i32>) {
+    let mut bruh = median_crab(&crabs);
+    loop {
+        let prev = calc_fuel_consumption_2(&crabs, bruh);
+        let next = calc_fuel_consumption_2(&crabs, bruh + 1);
+        if next <= prev {
+            bruh += 1
+        } else {
+            break;
+        }
+    }
+    loop {
+        let prev = calc_fuel_consumption_2(&crabs, bruh);
+        let next = calc_fuel_consumption_2(&crabs, bruh - 1);
+        if next <= prev {
+            bruh -= 1
+        } else {
+            break;
+        }
+    }
+    println!("{:?}", calc_fuel_consumption_2(&crabs, bruh))
+}
+
+fn main() {
+    let input = include_str!("../input/input.txt");
+    let input = input
+        .split(",")
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect_vec();
+
+    part1(input.clone());
+
+    part2(input.clone());
+}
