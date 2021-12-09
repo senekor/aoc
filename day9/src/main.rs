9b125c48a2ee6855ee1513f6af3326e431cab7bb
Remo Senekowitsch <remlause@gmail.com>
2021-12-09 19:42:07 +0100

    day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,108 @@
+use std::collections::HashSet;
+
+use itertools::*;
+
+fn is_low_point(input: &Vec<Vec<i32>>, i: usize, j: usize) -> bool {
+    let max_i = input.len() - 1;
+    let max_j = input[0].len() - 1;
+    let n1 = if i == 0 { i32::MAX } else { input[i - 1][j] };
+    let n2 = if j == 0 { i32::MAX } else { input[i][j - 1] };
+    let n3 = if i == max_i {
+        i32::MAX
+    } else {
+        input[i + 1][j]
+    };
+    let n4 = if j == max_j {
+        i32::MAX
+    } else {
+        input[i][j + 1]
+    };
+
+    let elem = input[i][j];
+
+    return elem < n1 && elem < n2 && elem < n3 && elem < n4;
+}
+
+fn part1(input: Vec<Vec<i32>>) {
+    let mut sum = 0;
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            if is_low_point(&input, i, j) {
+                // println!("{} {} {}", input[i][j], i, j);
+                sum += 1 + input[i][j]
+            }
+        }
+    }
+
+    println!("{:?}", &sum)
+}
+
+fn rec_find_basin(
+    input: &Vec<Vec<i32>>,
+    i: usize,
+    j: usize,
+    basin_locs: &mut HashSet<(usize, usize)>,
+) {
+    let max_i = input.len() - 1;
+    let max_j = input[0].len() - 1;
+
+    if i != 0 && input[i - 1][j] != 9 && !basin_locs.contains(&(i - 1, j)) {
+        basin_locs.insert((i - 1, j));
+        rec_find_basin(input, i - 1, j, basin_locs);
+    };
+    if j != 0 && input[i][j - 1] != 9 && !basin_locs.contains(&(i, j - 1)) {
+        basin_locs.insert((i, j - 1));
+        rec_find_basin(input, i, j - 1, basin_locs);
+    };
+    if i != max_i && input[i + 1][j] != 9 && !basin_locs.contains(&(i + 1, j)) {
+        basin_locs.insert((i + 1, j));
+        rec_find_basin(input, i + 1, j, basin_locs);
+    };
+    if j != max_j && input[i][j + 1] != 9 && !basin_locs.contains(&(i, j + 1)) {
+        basin_locs.insert((i, j + 1));
+        rec_find_basin(input, i, j + 1, basin_locs);
+    };
+}
+
+fn calc_basin_size(input: &Vec<Vec<i32>>, i: usize, j: usize) -> usize {
+    let mut basin_locs = HashSet::new();
+    basin_locs.insert((i, j));
+
+    rec_find_basin(input, i, j, &mut basin_locs);
+
+    basin_locs.len()
+}
+
+fn part2(input: Vec<Vec<i32>>) {
+    let mut basins = Vec::new();
+    for i in 0..input.len() {
+        for j in 0..input[i].len() {
+            if is_low_point(&input, i, j) {
+                // println!("{} {} {}", input[i][j], i, j);
+                basins.push(calc_basin_size(&input, i, j))
+            }
+        }
+    }
+    basins.sort();
+    let res = basins[basins.len() - 3..basins.len()]
+        .into_iter()
+        .fold(1, |acc, n| acc * n);
+
+    println!("{:?}", res)
+}
+
+fn main() {
+    let input = include_str!("../input/input.txt");
+    let input = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|n| n.to_string().parse::<i32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    part1(input.clone());
+
+    part2(input.clone());
+}
