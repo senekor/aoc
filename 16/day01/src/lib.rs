6558eb8b4e5594b114b4762506f09ee8b9f5ea84
Remo Senekowitsch <remlause@gmail.com>
2022-02-22 12:04:11 +0100

    16-01 Solve part 2

--- 16/day01/src/lib.rs
+++ 16/day01/src/lib.rs
@@ -16,3 +16,17 @@ pub fn part1(input: &str) -> i32 {
 
     santa.get_distance()
 }
+
+pub fn part2(input: &str) -> i32 {
+    let mut santa = Santa::default();
+    let instructions = input
+        .split(", ")
+        .map(|instr| instr.parse().unwrap())
+        .collect::<Vec<_>>();
+
+    for instruction in instructions {
+        santa.execute(instruction);
+    }
+
+    santa.get_first_visited_twice_distance()
+}
