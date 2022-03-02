670c3470655d4e5010ffd8a05cd936320688c88e
Remo Senekowitsch <remlause@gmail.com>
2022-03-02 22:45:16 +0100

    17-02 Solve part 1 & 2

---
+++ 17/day02/src/lib.rs
@@ -0,0 +1,33 @@
+use itertools::Itertools;
+
+pub fn part1(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split_ascii_whitespace()
+                .map(|num| num.parse::<i32>().unwrap());
+            nums.clone().max().unwrap() - nums.clone().min().unwrap()
+        })
+        .sum()
+}
+
+pub fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split_ascii_whitespace()
+                .map(|num| num.parse::<i32>().unwrap());
+            nums.clone()
+                .cartesian_product(nums)
+                .find_map(|(n1, n2)| match () {
+                    _ if n1 == n2 => None,
+                    _ if n1 % n2 == 0 => Some(n1 / n2),
+                    _ if n2 % n1 == 0 => Some(n2 / n1),
+                    _ => None,
+                })
+                .unwrap()
+        })
+        .sum()
+}
