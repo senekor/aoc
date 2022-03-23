923ad9e1c3d25b674e8908cfb15039739a90bc5a
Remo Senekowitsch <remlause@gmail.com>
2022-03-23 14:52:11 +0100

    21-01 Move input data to separate directory

--- 21/day01/src/main.rs
+++ 21/day01/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("../input/input.txt");
     let (res_part1, res_part2) = day01::lib_main(input);
     println!("{}", res_part1);
     println!("{}", res_part2);
