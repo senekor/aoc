ef77420759e2ea6b65f041f7e440f34c0b635b62
Remo Senekowitsch <contact@remlse.dev>
2022-04-21 21:45:15 +0200

    15-04 Improve code sytle

--- 15/day04/src/main.rs
+++ 15/day04/src/main.rs
@@ -1,15 +1,15 @@
-fn collision(input: &str, prefix: &str) {
+fn collision(input: &str, prefix: &str) -> u32 {
     for i in 0.. {
         let hash = format!("{:x}", md5::compute(format!("{}{}", input, i)));
-        if hash.get(0..prefix.len()).unwrap() == prefix {
-            println!("{}", i);
-            break;
+        if hash.starts_with(prefix) {
+            return i;
         }
     }
+    panic!()
 }
 
 fn main() {
     let input = include_str!("../input/input.txt");
-    collision(input, "00000");
-    collision(input, "000000");
+    println!("{}", collision(input, "00000"));
+    println!("{}", collision(input, "000000"));
 }
