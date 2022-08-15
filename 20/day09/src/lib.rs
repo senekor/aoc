7c6fddabd22a8635851f1071548ab692502d1dd9
Remo Senekowitsch <contact@remlse.dev>
2022-08-15 19:29:37 +0200

    20.09 Solve part 1 & 2

---
+++ 20/day09/src/lib.rs
@@ -0,0 +1,46 @@
+use std::collections::VecDeque;
+
+pub fn part1(input: &str, preamble_size: usize) -> i64 {
+    let mut preamble = VecDeque::new();
+    preamble.reserve(preamble_size);
+    for num in input.lines().map(|s| s.parse::<i64>().unwrap()) {
+        if preamble.len() < preamble_size {
+            preamble.push_back(num);
+            continue;
+        }
+
+        if preamble.iter().all(|n| !preamble.contains(&(num - n))) {
+            return num;
+        }
+
+        preamble.pop_front();
+        preamble.push_back(num);
+    }
+
+    panic!("not found")
+}
+
+pub fn part2(input: &str, preamble_size: usize) -> i64 {
+    let target = part1(input, preamble_size);
+    let nums: Vec<i64> = input.lines().map(|s| s.parse::<i64>().unwrap()).collect();
+
+    let (mut i, mut j) = (0, 1);
+
+    while j <= nums.len() {
+        let range = &nums[i..j];
+        let contig_sum = range.iter().sum::<i64>();
+
+        if contig_sum == target {
+            return range.iter().min().unwrap() + range.iter().max().unwrap();
+        }
+
+        match () {
+            _ if contig_sum == target => return nums[i] + nums[j - 1],
+            _ if contig_sum < target => j += 1,
+            _ if i + 1 == j => (i, j) = (i + 1, j + 1),
+            _ => i += 1,
+        }
+    }
+
+    panic!("not found")
+}
---
+++ 20/day09/tests/integration.rs
@@ -0,0 +1,25 @@
+use day09::{part1, part2};
+
+#[test]
+fn test_part1_sample() {
+    let input = include_str!("../input/sample.txt");
+    assert_eq!(part1(input, 5), 127);
+}
+
+#[test]
+fn test_part1() {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part1(input, 25), 167829540);
+}
+
+#[test]
+fn test_part2_sample() {
+    let input = include_str!("../input/sample.txt");
+    assert_eq!(part2(input, 5), 62);
+}
+
+#[test]
+fn test_part2() {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part2(input, 25), 28045630);
+}
