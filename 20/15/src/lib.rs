31682bc743318edcd3694c60a61f680f0716e9d4
Remo Senekowitsch <contact@remlse.dev>
2023-04-18 08:48:32 +0200

    20.15 increase performance

--- 20/15/src/lib.rs
+++ 20/15/src/lib.rs
@@ -1,24 +1,28 @@
-use std::collections::HashMap;
-
 fn game(input: &str, limit: usize) -> usize {
     let initial_numbers = input
         .split(',')
         .map(|s| s.parse().unwrap())
         .collect::<Vec<_>>();
     let n = initial_numbers.len();
-    let mut num_turn = HashMap::new();
+    let mut num_turn = Vec::with_capacity(n + 1);
     for (i, init_num) in initial_numbers.into_iter().enumerate() {
-        num_turn.insert(init_num, i);
+        if num_turn.len() <= init_num {
+            num_turn.resize(init_num + 1, 0);
+        }
+        num_turn[init_num] = i + 1;
     }
     let mut call = 0;
-    for i in n..limit - 1 {
-        if !num_turn.contains_key(&call) {
-            num_turn.insert(call, i);
-            call = 0
+    for i in n + 1..limit {
+        if num_turn.len() <= call {
+            num_turn.resize(call + 1, 0);
+        }
+        if num_turn[call] == 0 {
+            num_turn[call] = i;
+            call = 0;
         } else {
             let prev = call;
-            call = i - num_turn[&prev];
-            num_turn.insert(prev, i);
+            call = i - num_turn[prev];
+            num_turn[prev] = i;
         }
     }
     call
