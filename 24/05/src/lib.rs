1567c802411db601d14f1e6b8033a8097fcf8e43
Remo Senekowitsch <remo@buenzli.dev>
2024-12-25 23:19:01 +0100

    24.05 solve part 2

--- 24/05/src/lib.rs
+++ 24/05/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn part1(input: &str) -> i32 {
+fn parse(input: &str) -> (HashMap<&str, Vec<&str>>, Vec<Vec<&str>>) {
     let (page_ordering_rules, updates) = input.split_once("\n\n").unwrap();
 
     let page_ordering_rules = {
@@ -17,6 +17,12 @@ pub fn part1(input: &str) -> i32 {
         .lines()
         .map(|l| l.split(',').collect::<Vec<_>>())
         .collect::<Vec<_>>();
+
+    (page_ordering_rules, updates)
+}
+
+pub fn part1(input: &str) -> i32 {
+    let (page_ordering_rules, updates) = parse(input);
 
     let mut sum = 0;
     'updates_loop: for update in updates {
@@ -39,6 +45,38 @@ pub fn part1(input: &str) -> i32 {
     sum
 }
 
-pub fn part2(input: &str) -> usize {
-    0
+pub fn part2(input: &str) -> i32 {
+    let (page_ordering_rules, updates) = parse(input);
+
+    let mut sum = 0;
+    for mut update in updates {
+        let mut was_incorrect = false;
+        let mut i = 0;
+        'pages_loop: while i < update.len() {
+            let page = update[i];
+            let Some(deps) = page_ordering_rules.get(page) else {
+                // page has no deps, no need to check anything
+                i += 1;
+                continue 'pages_loop;
+            };
+            for k in 0..i {
+                #[allow(clippy::borrow_deref_ref, reason = "won't compile otherwise")]
+                let prev_page = &&*update[k];
+                if deps.contains(prev_page) {
+                    was_incorrect = true;
+                    // fix update
+                    update.remove(k);
+                    update.insert(i, prev_page);
+                    // between k..i may be dependencies of prev_page so we need
+                    // to run the check loop for the same index i again.
+                    continue 'pages_loop;
+                }
+            }
+            i += 1;
+        }
+        if was_incorrect {
+            sum += update[update.len() / 2].parse::<i32>().unwrap();
+        }
+    }
+    sum
 }
--- 24/05/tests/integration.rs
+++ 24/05/tests/integration.rs
@@ -12,11 +12,11 @@ fn test_part1_sample() {
 
 #[test]
 fn test_part2_sample() {
-    assert_eq!(part2(SAMPLE), Fail);
+    assert_eq!(part2(SAMPLE), 123);
 }
 
 utils::solution!(
     aoc_24_05;
     6260;
-    TODO;
+    5346;
 );
